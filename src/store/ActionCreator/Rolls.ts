706fc12749af627e50f0d871a6aee58b9ee3eec7
achabalov <35976320+achabalov@users.noreply.github.com>
2021-09-12 21:17:06 +0300

    Update Rolls.ts

--- src/store/ActionCreator/Rolls.ts
+++ src/store/ActionCreator/Rolls.ts
@@ -1,21 +1,3 @@
-// import { Dispatch } from "redux";
-// import { RollsAction, RollsActionTypes } from "../../types";
-// import rolls from '../../components/API/mainPage.json';
-
-// export const fetchRolls = () => {
-//     return async (dispatch: Dispatch<RollsAction>) => {
-//         try {
-//             dispatch({type: RollsActionTypes.FETCH_ROLLS})
-
-//             dispatch({type: RollsActionTypes.FETCH_ROLLS_SUCCESS, 
-//                 payload: rolls})
-//         } catch(e) {
-//             dispatch({type: RollsActionTypes.FETCH_ROLLS_ERROR, 
-//                 payload: 'Произошла ошибка при загрузке роллов'})
-//         }
-//     }
-// }
-
 import { Dispatch } from "redux";
 import { RollsAction, RollsActionTypes } from "../../types";
 import rolls from '../../components/API/mainPage.json';
